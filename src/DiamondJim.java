9473b4d6e53a8566368d4bb84922864424c68e6d
KathyOlsen <katherinekolsen@verizon.net>
2019-09-10 10:19:52 -0400

    Initial commit

---
+++ src/DiamondJim.java
@@ -0,0 +1,7 @@
+public class DiamondJim {
+    public static void main(String[] args){
+        //print a diamond shape made of asterisks
+        System.out.println("  * \n * *\n*   *\n * *\n  *");
+
+    }
+}
